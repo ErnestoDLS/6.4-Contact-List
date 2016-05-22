ea2ea556eff029c56530566ccb9aefaa0ed21d09
Ernesto De Los Santos <netos_id@yahoo.com>
2016-05-22 17:59:56 -0500

    nothing rendering yet

--- modules/Contacts.js
+++ modules/Contacts.js
@@ -1,39 +1,24 @@
 import React from 'react'
 
 
-var ContactData =  {
-    image: "path/to/image.jpg",
-    name: "Thor",
-    email: "email",
-    number: "(555)555-5555",
-    location: "Asgard",
-    {
-      image: "path/to/image.jpg",
-      name: "Peter Parker",
-      email: "spidey_luvs-MJ@gmail.com",
-      number: "(555)555-5555",
-      location: "Brooklyn,New York",
-    },
-    {
-      image: "path/to/image.jpg",
-      name: "Goku",
-      email: "icantEATthis@gmail.com",
-      number: "(555)555-5555",
-      location: "Planet Vegeta"
+var contacts = [
+    {"id": 0, "firstName": "Peter", "lastName": "Parker", "number": "555-555-5555", "email": "spidey_luvs-MJ@gmail.com", "city": "Brooklyn, NY", "pic": "Peter_parker.jpg"},
+    {"id": 1, "firstName": "Bill", "lastName": "Murray", "number": "555-555-5555", "email": "ghostbusters_rulz@gmail.com", "city": "San Antonio, TX", "pic": "Bill_muarry.jpg"},
+    {"id": 2, "firstName": "Bruce", "lastName": "Lee", "number": "555-555-5555", "email": "little__dragon@gmail.com", "city": "Seattle, WA", "pic": "Bruce_lee.jpg",},
+    {"id": 3, "firstName": "Tom", "lastName": "Brady","number": "555-555-5555", "email": "GOAT@gmail.com", "city": "Boston, MA", "pic": "Tom_brady.jpg"},
 
-    },
-    {
-      image: "path/to/image.jpg",
-      name: "Gracie",
-      email: "graciethegolden@gmail.com",
-      number: "(555)555-5555",
-      location: "San Antonio, Texas"
+    exports.findAll = function (req, res, next) {
+        var name = req.query.name;
+        if (name) {
+            res.send(employees.filter(function(employee) {
+                return (employee.firstName + ' ' + employee.lastName).toLowerCase().indexOf(name.toLowerCase()) > -1;
+            }));
+        } else {
+            res.send(employees);
+        }
     }
 
-}
-
-export default React.createClass({
-  render(){
-    return <h1>Contacts</h1>
-  }
-})
+    exports.findById = function (req, res, next) {
+        var id = req.params.id;
+        res.send(employees[id]);
+    };
